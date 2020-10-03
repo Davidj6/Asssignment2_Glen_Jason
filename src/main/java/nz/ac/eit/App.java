6c294ff87542457ba88cb99791e346e6f39b94f0
Davidj6 <sweetasbaby@hotmail.com>
2020-10-03 14:48:04 +1300

    Player welcome message and rules.

--- src/main/java/nz/ac/eit/App.java
+++ src/main/java/nz/ac/eit/App.java
@@ -1,13 +1,16 @@
 package nz.ac.eit;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Hello!" );
+        System.out.println( "Welcome to Dice Blackjack!\n" +
+                "Rules:\n" +
+                "Player gets two dice rolls with scores added together.\n" +
+                "With the aim of getting 21 if u go over you lose.\n" +
+                "You have option to roll again if u wish or hold on first score.\n" +
+                "Then computer will have its turn.\n" +
+                "Closest to 21 wins!" );
     }
 }
+
