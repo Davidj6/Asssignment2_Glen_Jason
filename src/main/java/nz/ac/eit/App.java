c5a3b3355fc66fa8e94cbed51c7eaac1f275fc01
Davidj6 <sweetasbaby@hotmail.com>
2020-10-05 22:57:22 +1300

    Issue #19 Display both scores and show winner. Refactored code to make cleaner and better organised.

--- src/main/java/nz/ac/eit/App.java
+++ src/main/java/nz/ac/eit/App.java
@@ -1,6 +1,7 @@
 package nz.ac.eit;
 
 import java.util.Scanner;
+import static java.lang.System.exit;
 
 public class App
 {
@@ -12,50 +13,71 @@ public class App
                 "Player gets two dice rolls with scores added together.\n" +
                 "With the aim of getting 21 if u go over you lose.\n" +
                 "You have option to roll again if u wish or hold on first score.\n" +
+                "If player gets 21 they are automatic winner.\n" +
+                "If player gets over 21 they are automatic loser.\n" +
+                "Player can roll two dice as many times as they want.\n" +
                 "Then computer will have its turn.\n" +
                 "Closest to 21 wins!\n" );
 
+        System.out.println("Players turn:\n");
+
         //Players first dice rolls.
-        Integer dice1 =  (int)Math.floor(Math.random() * 6 + 1);
-        Integer dice2 = (int)Math.floor(Math.random()*6+1);
-        Integer totalDice1 = dice1 + dice2;
-        System.out.println("Players first dice rolls = " + totalDice1);
+        int playerTotal = 0;
+        playerTotal += rollDice();
+        System.out.println("Player total score is: " + playerTotal);
 
-        //Player choice.
-        System.out.println("Do you want to roll again(y/n)?");
-        Scanner scanner = new Scanner(System.in);
-        String playerChoice = scanner.nextLine();
+        //Player choice and dice rolls.
+        String playerChoice;
+        do {
+            //Ask player input.
+            System.out.println("Do you want to roll again(y/n)?");
+            Scanner scanner = new Scanner(System.in);
+            playerChoice = scanner.nextLine();
 
-        //If statement for outcome of players choice.
-        Integer totalDice2 = 0;
-        if (playerChoice.equals("y")){
-            Integer dice3 =  (int)Math.floor(Math.random() * 6 + 1);
-            Integer dice4 = (int)Math.floor(Math.random()*6+1);
-            totalDice2 = dice3 + dice4;
-            System.out.println("Players second dice rolls = " + totalDice2);
+            //Roll dice if player wants to 'hit'.
+            if (playerChoice.equals("y")) {
+                playerTotal += rollDice();
+                System.out.println("Player total score is: " + playerTotal);
+            }
+
+            //Player automatic win or lose conditions.
+            if (playerTotal == 21) {
+                System.out.println(playerTotal + " PLAYER WINS!!! Congratulations.");
+                exit(0);
+            } else if (playerTotal >= 21) {
+                System.out.println(playerTotal + " PLAYER LOSES!!! Sorry try again.");
+                exit(0);
+            }
+        } while (playerChoice.equals("y"));
+
+        System.out.println("Computers turn:\n");
+
+        //Computers choice to roll while there scores is less than player.
+        int computerTotal = 0;
+        do {
+            computerTotal += rollDice();
+        } while (computerTotal <= playerTotal);
+
+        //Computer automatic win or lose conditions.
+        System.out.println("Computers total score is: " + computerTotal);
+        if (computerTotal > 21) {
+            System.out.println(computerTotal + "COMPUTER LOSES!!!");
+        } else if (computerTotal == 21 || computerTotal > playerTotal) {
+            System.out.println(computerTotal + "COMPUTER WINS!!!");
         }
-        Integer playerTotalDiceScore = totalDice1 + totalDice2;
-        System.out.println("Player total score is: " + playerTotalDiceScore);
-
-        //Computers first dice rolls.
-        System.out.println("Computers turn.\n");
-        Integer dice5 =  (int)Math.floor(Math.random() * 6 + 1);
-        Integer dice6 = (int)Math.floor(Math.random()*6+1);
-        Integer totalDice3 = dice5 + dice6;
-        System.out.println("Computers first dice rolls = " + totalDice3);
-
-        //Computers choice.
-        Integer totalDice4 = 0;
-        if (totalDice3 <= 11){
-            Integer dice7 =  (int)Math.floor(Math.random() * 6 + 1);
-            Integer dice8 = (int)Math.floor(Math.random()*6+1);
-            totalDice4 = dice7 + dice8;
-            System.out.println("Computers second dice rolls = " + totalDice4);
+    }
+    //Roll dice function
+    private static int rollDice()
+    {
+        int dice = 0;
+        int total = 0;
+        for (int cnt = 0; cnt < 2; cnt++)
+        {
+            dice = (int) Math.floor(Math.random() * 6 + 1);
+            System.out.println("Dice roll = " + dice);
+            total += dice;
         }
-        Integer computerTotalDiceScore = totalDice3 + totalDice4;
-        System.out.println("Computers total score is: " + computerTotalDiceScore);
-
-
+        return total;
     }
 }
 
