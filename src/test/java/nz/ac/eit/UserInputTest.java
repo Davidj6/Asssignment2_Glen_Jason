f131e276adadcb8705b7fb894f3e09961ba4e3f1
THOMSG7 <geetee8teen@gmail.com>
2020-10-14 18:00:34 +1300

    collectInput_withNo_shouldReturnNoSupportedInput and collectInput_withYes_shouldReturnYesSupportedInput tests

--- src/test/java/nz/ac/eit/UserInputTest.java
+++ src/test/java/nz/ac/eit/UserInputTest.java
@@ -10,4 +10,18 @@ public class UserInputTest {
         UserInput userInput=new UserInput();
         userInput.collectInput("NotYesOrNo");
     }
+
+    @Test
+    public void collectInput_withYes_shouldReturnYesSupportedInput() throws InputNotSupportedException {
+        UserInput userInput=new UserInput();
+        SupportedInput input=userInput.collectInput("yes");
+        assertEquals(SupportedInput.YES,input);
+    }
+
+    @Test
+    public void collectInput_withNo_shouldReturnNoSupportedInput() throws InputNotSupportedException {
+        UserInput userInput=new UserInput();
+        SupportedInput input=userInput.collectInput("no");
+        assertEquals(SupportedInput.NO,input);
+    }
 }
